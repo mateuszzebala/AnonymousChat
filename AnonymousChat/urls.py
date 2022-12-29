77d0fcd7b984a01b09cca46da45faef2a4af5968
mateuszzebala <mateusz.zebala.pl@gmail.com>
2022-12-29 19:08:24 +0100

    fix #2 of static url

--- AnonymousChat/urls.py
+++ AnonymousChat/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.contrib.staticfiles.views import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^static/(?P<path>.*)$', serve, {"document_root": settings.STATIC_ROOT}),
     path('', include('Main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
